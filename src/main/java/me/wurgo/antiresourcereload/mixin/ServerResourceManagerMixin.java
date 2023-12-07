c55501c78ef4bda13b118cc0d03d1d488f3095bc
contaria <kingcontaria@gmail.com>
2023-12-07 03:55:45 +0100

    consistent mixin formatting also changed back the redirect to an inject-cancel because it better covers what we want to do, which is stopping the cancelling of anything to do with our cached resource manager, including potential stuff other mods could put in the close method that would still run with the redirect

--- src/main/java/me/wurgo/antiresourcereload/mixin/ServerResourceManagerMixin.java
+++ src/main/java/me/wurgo/antiresourcereload/mixin/ServerResourceManagerMixin.java
@@ -1,27 +1,25 @@
 package me.wurgo.antiresourcereload.mixin;
 
 import me.wurgo.antiresourcereload.AntiResourceReload;
-import net.minecraft.resource.ReloadableResourceManager;
 import net.minecraft.resource.ServerResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.concurrent.ExecutionException;
 
 @Mixin(ServerResourceManager.class)
 public abstract class ServerResourceManagerMixin {
-    @Redirect(
+    @Inject(
             method = "close",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/resource/ReloadableResourceManager;close()V"
-            )
+            at = @At("HEAD"),
+            cancellable = true
     )
-    private void antiresourcereload_keepOpened(ReloadableResourceManager instance) throws ExecutionException, InterruptedException {
+    private void keepCachedResourcesOpen(CallbackInfo ci) throws ExecutionException, InterruptedException {
         // noinspection ConstantConditions - I think mixin is confusing for intellij here
         if (AntiResourceReload.cache == null || (Object) this != AntiResourceReload.cache.get()) {
-            instance.close();
+            ci.cancel();
         }
     }
 }
